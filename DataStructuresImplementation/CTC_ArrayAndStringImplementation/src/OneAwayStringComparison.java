795c64369b72c405917d7e7cfce2b82447e1d8df
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-20 17:24:06 -0400

    CTC Array and String Implementation

--- DataStructuresImplementation/CTC_ArrayAndStringImplementation/src/OneAwayStringComparison.java
+++ DataStructuresImplementation/CTC_ArrayAndStringImplementation/src/OneAwayStringComparison.java
@@ -6,42 +6,49 @@ public class OneAwayStringComparison {
 	 *            replace. given two strings, write a function to check if they
 	 *            are one edit (or zero edits) away
 	 */
-	public void StringComparator(String str1, String str2) {
+	public boolean StringComparator(String str1, String str2) {
 		if (str1.length() - str2.length() == 1) {
+			if(StringInsertChecker(str1, str2))
+				return true;
 		}
-		if (str2.length() - str2.length() == 1) {
+		if (str2.length() - str1.length() == 1) {
+			if(StringInsertChecker(str2, str1)){
+				return true;
+			}
 		}
+		return false;
 	}
 
 	/**
 	 * @param str1
 	 * @param str2
-	 * assumption is that str1.length() < str2.length()
+	 *            assumption is that str1.length() < str2.length()
 	 */
 	public boolean StringInsertChecker(String str1, String str2) {
-	int index1 = 0,index2 = 0;
-	int diffCounter=0;
-		while(index1 < str1.length() && index2<str2.length()){
+		int index1 = 0, index2 = 0;
+		int diffCounter = 0;
+		while (index1 < str1.length() && index2 < str2.length()) {
 			char ch1 = str1.charAt(index1);
 			char ch2 = str2.charAt(index2);
-			
-			if(ch1!=ch2){
+
+			if (ch1 != ch2) {
 				diffCounter++;
 				index2++;
-			}else{
+			} else {
 				index1++;
 				index2++;
 			}
-			if(diffCounter>1){
+			if (diffCounter > 1) {
 				return false;
 			}
-		}return true;
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		OneAwayStringComparison obj = new OneAwayStringComparison();
-		System.out.println(obj.StringInsertChecker("abdc", "abc"));
+		System.out.println(obj.StringComparator("anmbc", "abc"));
 	}
 
 }
