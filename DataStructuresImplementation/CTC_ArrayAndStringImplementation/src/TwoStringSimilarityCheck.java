31a1a98fd23a299c563c6bf8f6913b67e076dfae
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-19 23:49:53 -0400

    CTC Array and String Implementation

--- DataStructuresImplementation/CTC_ArrayAndStringImplementation/src/TwoStringSimilarityCheck.java
+++ DataStructuresImplementation/CTC_ArrayAndStringImplementation/src/TwoStringSimilarityCheck.java
@@ -1,10 +1,14 @@
-
+import java.util.HashMap;
 public class TwoStringSimilarityCheck {
 
 	/**
 	 * @param args
 	 * Checking if two strings are permutations of each other
 	 */
+	
+	public void StringUniqueChecker(String str1 , String str2){
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
