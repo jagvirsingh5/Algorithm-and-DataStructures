337346bf158b5627f94fdabbe6d7c119351a5dae
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-20 17:50:03 -0400

    CTC Array and String Implementation

--- DataStructuresImplementation/CTC_ArrayAndStringImplementation/src/OneAwayStringComparison.java
+++ DataStructuresImplementation/CTC_ArrayAndStringImplementation/src/OneAwayStringComparison.java
@@ -8,14 +8,20 @@ public class OneAwayStringComparison {
 	 */
 	public boolean StringComparator(String str1, String str2) {
 		if (str1.length() - str2.length() == 1) {
-			if(StringInsertChecker(str1, str2))
+			if (StringInsertChecker(str1, str2))
 				return true;
 		}
 		if (str2.length() - str1.length() == 1) {
-			if(StringInsertChecker(str2, str1)){
+			if (StringInsertChecker(str2, str1)) {
 				return true;
 			}
 		}
+		if (str1.length() == str2.length()) {
+			if (StringReplaceChecker(str1, str2)) {
+				return true;
+			}
+		}
+
 		return false;
 	}
 
@@ -45,10 +51,30 @@ public class OneAwayStringComparison {
 		return true;
 	}
 
+	public boolean StringReplaceChecker(String str1, String str2) {
+
+		int index1 = 0, index2 = 0;
+		int sumOfDiff = 0;
+		while (index1 < str1.length() && index2 < str2.length()) {
+			char ch1 = str1.charAt(index1);
+			char ch2 = str2.charAt(index2);
+			if (ch1 != ch2) {
+				sumOfDiff++;
+			}
+			index1++;
+			index2++;
+			if (sumOfDiff > 1) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		OneAwayStringComparison obj = new OneAwayStringComparison();
-		System.out.println(obj.StringComparator("anmbc", "abc"));
+		System.out.println(obj.StringComparator("bale", "faKe"));
 	}
 
 }
