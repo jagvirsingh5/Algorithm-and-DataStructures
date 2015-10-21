eb55ddd81a3a043db3917b32c2fc6f11580ad979
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-20 20:32:50 -0400

    CTC Array and String Implementation

--- DataStructuresImplementation/CTC_ArrayAndStringImplementation/src/StringCompression.java
+++ DataStructuresImplementation/CTC_ArrayAndStringImplementation/src/StringCompression.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class StringCompression {
 
@@ -8,15 +9,36 @@ public class StringCompression {
 	 *            counting sequences of repeating characters. Given "aaabbbccc"
 	 *            it should return "a3b3c3".
 	 */
-	
-	public void StringCompressor(String str){
+
+	public void StringCompressor(String str1) {
+		String str = StringSorter(str1);
 		char[] ch = str.toCharArray();
-		Arrays.sort(ch);
-		String str = ch;
-		System.out.println();
+		HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
+		for(int i =0 ; i< str.length();i++){
+			int value=1;
+			if(!hm.containsKey(ch[i])){
+				hm.put(ch[i], value);
+			}
+			else{
+				hm.put(ch[i], (hm.get(ch[i])+1));
+			}
+		}
 	}
+	
+	public String StringSorter(String str){
+		 
+		 char[] ch = str.toCharArray();
+		 StringBuilder sb = new StringBuilder();
+		 Arrays.sort(ch);
+		 for(char c : ch ){sb.append(c);}
+		 str= sb.toString();
+		 return str;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		StringCompression obj = new StringCompression();
+		obj.StringCompressor("jagvir");
 
 	}
 
