a85668aeb2848eb0883d0051c45ac893f9cc1413
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-22 00:04:43 -0400

    CTC LinkdList Implementation

--- DataStructuresImplementation/CTC_Linked_list_Implementation/src/LinkedListPalindromeWithHalfReversal.java
+++ DataStructuresImplementation/CTC_Linked_list_Implementation/src/LinkedListPalindromeWithHalfReversal.java
@@ -4,6 +4,22 @@ public class LinkedListPalindromeWithHalfReversal {
 	/**
 	 * @param args
 	 */
+	public class Node {
+		Node next;
+		int data;
+	}
+
+	public void disp(Node head) {
+		System.out.println("list is :- ");
+		Node c = head;
+		while (c != null) {
+			System.out.print(c.data + " ");
+			c = c.next;
+		}
+		System.out.println();
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
