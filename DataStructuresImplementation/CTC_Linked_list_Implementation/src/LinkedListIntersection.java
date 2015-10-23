6999ac1d624ce69960217b5bad1f6bc5178454cc
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-22 23:01:50 -0400

    CTC LinkedList mods

--- DataStructuresImplementation/CTC_Linked_list_Implementation/src/LinkedListIntersection.java
+++ DataStructuresImplementation/CTC_Linked_list_Implementation/src/LinkedListIntersection.java
@@ -23,15 +23,33 @@ public class LinkedListIntersection {
 		System.out.println("length of list is :- " + length);
 	}
 
-	public void diffFinder(returnDetail list1, returnDetail list2) {
+	public boolean diffFinder(returnDetail list1, returnDetail list2) {
 		int lengthList1 = list1.length;
 		int lengthList2 = list2.length;
 		Node node1 = list1.returnNode;
 		Node node2 = list2.returnNode;
-		
-		if(lengthList1>lengthList2){
-			
+		System.out.println(lengthList1 + " " + lengthList2);
+		int diff = Math.abs(lengthList1 - lengthList2);
+		System.out.println(diff);
+		if (lengthList1 > lengthList2) {
+
+			while (diff != 0) {
+
+				node1 = node1.next;
+				diff--;
+			}
+
+			while (node1.next != null && node2.next != null) {
+				System.out.println(node1.data + "    " + node2.data);
+				if (node1.next == node2.next) {
+					return true;
+				}
+				node1 = node1.next;
+				node2 = node2.next;
+			}
+
 		}
+		return false;
 	}
 
 	private returnDetail linkedListCreator(int[] list) {
@@ -60,17 +78,38 @@ public class LinkedListIntersection {
 		return obj;
 	}
 
-	public void runner(int[] list) {
-		displayList(linkedListCreator(list));
-
+	public void runner(int[] list1, int[] list2) {
+		returnDetail ret1 = linkedListCreator(list1);
+		returnDetail ret2 = linkedListCreator(list2);
+		int len1 = ret1.length;
+		int len2 = ret2.length;
+		// System.out.println(len1 + " " + len2);
+		Node head1 = ret1.returnNode;
+		Node head2 = ret2.returnNode;
+		Node end1 = head1;
+		Node end2 = head2;
+		while (end1.next != null) {
+			end1 = end1.next;
+		}
+		while (end2.next != null) {
+			end2 = end2.next;
+		}
+		// Node n = new Node();
+		// n.data = 9;
+		// end1.next = n;
+		// end2.next = n;
+		// ret1.length++;
+		// ret2.length++;
+		// System.out.println(ret1.length + " " + ret2.length);
+		System.out.println(diffFinder(ret1, ret2));
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedListIntersection obj = new LinkedListIntersection();
-		int[] list = { 1, 2, 3, 4, 5, 6, 5, 6, 7, 8, 9, 1, 2, 22, 3, 333, 4455,
-				234 };
-		obj.runner(list);
+		int[] list1 = { 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+		int[] list2 = { 1, 2, 3, 4, 5, 6 };
+		obj.runner(list1, list2);
 	}
 
 }
