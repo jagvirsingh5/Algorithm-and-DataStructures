39925baccb24c2349d513c7f4ca681a8638dd1e0
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-24 14:25:44 -0400

    CTC Stack and Queue start

---
+++ DataStructuresImplementation/CTC_Tree/src/TreeImplementation.java
@@ -0,0 +1,46 @@
+public class TreeImplementation {
+
+	/**
+	 * @param args
+	 */
+	private class Node {
+		Node leftNode;
+		Node rightNode;
+		int data;
+	}
+
+	private void insert(Node node, int data) {
+		if (node == null) {
+			node = new Node();
+			node.data = data;
+		} else {
+			if (data < node.data) {
+				insert(node.leftNode, data);
+
+			} else {
+				insert(node.rightNode, data);
+			}
+		}
+	}
+
+	private boolean lookup(Node node, int data) {
+		if (node == null) {
+			return false;
+		}
+		if (data == node.data) {
+			return true;
+		}
+		if (data < node.data) {
+			lookup(node.leftNode, data);
+		} else {
+			lookup(node.rightNode, data);
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
