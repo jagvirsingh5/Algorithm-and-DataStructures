c821d291c4c87d34c0ccbb0cf4b986094a251e5b
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-26 00:22:51 -0400

    CTC Tree

--- DataStructuresImplementation/CTC_Tree/src/bitPalindrome.java
+++ DataStructuresImplementation/CTC_Tree/src/bitPalindrome.java
@@ -1,12 +1,38 @@
-import java.util.NoSuchElementException;
-import java.util.Iterator;
-
 public class bitPalindrome {
 
 	/**
 	 * @param args
 	 */
 
+	private String convertToString(int data) {
+		StringBuilder sb = new StringBuilder();
+		boolean flag = true;
+		while (flag) {
+			int rem = data % 2;
+			data = data / 2;
+			sb = sb.append(rem);
+			if (data == 0) {
+				flag = false;
+			}
+		}
+		String ret = sb.toString();
+
+		return ret;
+	}
+
+	private boolean isPalindrome(int data) {
+		String str = convertToString(data);
+		char[] ch = str.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] != ch[(ch.length-1) - i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
+		bitPalindrome obj = new bitPalindrome();
+		System.out.println(obj.isPalindrome(10));
 	}
 }
