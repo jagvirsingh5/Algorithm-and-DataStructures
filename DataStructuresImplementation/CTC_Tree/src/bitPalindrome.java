6230c966f25b73ac3baf984e37a3ad0712b0a83e
Jagvir Singh <jagvirsingh5@gmail.com>
2015-10-26 21:21:49 -0400

    CTC Tree

--- DataStructuresImplementation/CTC_Tree/src/bitPalindrome.java
+++ DataStructuresImplementation/CTC_Tree/src/bitPalindrome.java
@@ -24,15 +24,51 @@ public class bitPalindrome {
 		String str = convertToString(data);
 		char[] ch = str.toCharArray();
 		for (int i = 0; i < ch.length; i++) {
-			if (ch[i] != ch[(ch.length-1) - i]) {
+			if (ch[i] != ch[(ch.length - 1) - i]) {
 				return false;
 			}
 		}
 		return true;
 	}
 
+	private boolean isPalindromeBitManipulator(int data) {
+		int temp = data;
+		int rev = 0;
+		while (temp != 0) {
+			rev = rev << 1;
+			rev = rev | (temp & 1);
+			temp = temp >> 1;
+		}
+		System.out.println(rev + " " + data);
+		if (rev == data)
+			return true;
+		else
+			return false;
+	}
+
+	private int setBitFinder(int data) {
+		int temp = data;
+		int counter = 0;
+		while (temp != 0) {
+
+			if ((temp & 1) != 0)
+				counter++;
+			temp = temp >> 1;
+		}
+		return counter;
+	}
+
 	public static void main(String[] args) {
+
 		bitPalindrome obj = new bitPalindrome();
+		long starttime = System.currentTimeMillis();
 		System.out.println(obj.isPalindrome(10));
+		long endtime = System.currentTimeMillis();
+		System.out.println(endtime - starttime);
+		starttime = System.currentTimeMillis();
+		System.out.println(obj.isPalindromeBitManipulator(165));
+		endtime = System.currentTimeMillis();
+		System.out.println(endtime - starttime);
+		System.out.println("no of set digit in "+165+" is :- "+obj.setBitFinder(165));
 	}
 }
